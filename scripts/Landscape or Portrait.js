8733ced4e92057867323e33f7a91cc802ac54127
Max98620 <m.ingram2@share.epsb.ca>
2021-09-21 09:54:19 -0600

    Update

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -1,1 +1,28 @@
 console.log('You made it to the Landscape or portrait.js!');
+
+let stop = false;
+
+function javascript () {
+
+}
+
+function main() {
+  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
+   //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
+  firstNumber = document.getElementById("textfield1").value;
+  secondNumber = document.getElementById("textfield2").value;
+  //validate by alert or html text in p tag
+  document.getElementById("validityTest1").innerHTML = alert (testNAN(firstNumber));
+  document.getElementById("validityTest1").innerHTML = (testNAN(firstNumber));
+  document.getElementById("validityTest2").innerHTML = alert (testNAN(secondNumber));
+  document.getElementById("validityTest2").innerHTML = (testNAN(secondNumber));
+}
+
+function testNAN (number) {
+  if ( isNAN (number)) {//NaN are not values o REAL number system
+    stop = true;
+    return "Type a Real Number!"
+  } else {
+    return "Input Validated"
+  }
+}
