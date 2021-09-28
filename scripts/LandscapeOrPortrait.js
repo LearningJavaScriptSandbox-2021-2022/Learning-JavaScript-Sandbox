66b4f3f760aceb4d90ff4fca457c54c223269534
Max98620 <m.ingram2@share.epsb.ca>
2021-09-28 10:07:46 -0600

    update

--- scripts/LandscapeOrPortrait.js
+++ scripts/LandscapeOrPortrait.js
@@ -9,17 +9,35 @@ function javascript () {
 function main() {
   let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
    //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
-  firstNumber = document.getElementById("textfield1").value;
-  secondNumber = document.getElementById("textfield2").value;
+  firstNumber = document.getElementById("textField1").value;
+  secondNumber = document.getElementById("textField2").value;
   //validate by alert or html text in p tag
   document.getElementById("validityTest1").innerHTML = alert (testNAN(firstNumber));
   document.getElementById("validityTest1").innerHTML = (testNAN(firstNumber));
   document.getElementById("validityTest2").innerHTML = alert (testNAN(secondNumber));
   document.getElementById("validityTest2").innerHTML = (testNAN(secondNumber));
+  if (stop == true) {
+      document.getElementById("answer").innerHTML = "Try Again!"
+    } else {
+
+      document.getElementById("answer").innerHTML = Solution (firstNumber, secondNumber);
+
+  }
+}
+
+function Solution (firstNumber, secondNumber) {
+  if (firstNumber > secondNumber) {
+    return "You are in Portrait mode!"
+  }else if (firstNumber < secondNumber) {
+    return "You are in Landscape Mode!"
+
+  }else {
+    return "You are in Square Mode!"
+  }
 }
 
 function testNAN (number) {
-  if ( isNAN (number)) {//NaN are not values o REAL number system
+  if ( isNaN (number)) {//NaN are not values o REAL number system
     stop = true;
     return "Type a Real Number!"
   } else {
--- scripts/ModulusAssignments.js
+++ scripts/ModulusAssignments.js
@@ -1,1 +1,49 @@
 console.log('You made it to the Modulus assignments.js!');
+
+
+let stop = false;
+
+function javascript () {
+
+}
+
+
+function main () {
+  let firstNumber;
+
+  firstNumber = document.getElementById("textfield1").value;
+
+
+  document.getElementById("validityTest1").innerHTML = alert (testNaN (firstNumber) );
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+  if (stop == true) {
+      document.getElementById("answer").innerHTML = "Try Again!"
+    } else {
+
+      document.getElementById("answer").innerHTML = Solution (firstNumber);
+
+    }
+}
+
+function EvenOrOdd(firstNumber) {
+  firstNumber % 2
+}
+
+function testNaN(number) {
+  if (isNaN (number) ) {
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "input validated"
+  }
+}
+
+var result = EvenOrOdd();
+
+function Solution (firstNumber) {
+  if (result = 0) {
+    return "Odd"
+  }else {
+    return "even"
+  }
+}
