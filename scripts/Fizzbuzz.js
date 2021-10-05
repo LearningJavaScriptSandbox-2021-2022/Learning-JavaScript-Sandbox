0a19acbffb0546f63f91432223c3004a68073754
Max Ingram <Max98620@gmail.com>
2021-10-04 20:48:15 -0600

    update

--- scripts/Fizzbuzz.js
+++ scripts/Fizzbuzz.js
@@ -1,1 +1,36 @@
-console.log('You made it to the Fizzbuzz.js!');
+console.log('You made it to the FizzBuzz.js!');
+
+
+let stop = false;
+
+function javascript () {
+
+}
+
+
+function main() {
+  let firstNumber;
+
+  firstNumber = Number(document.getElementById("textfield1").value);
+
+  document.getElementById("validityTest1").innerHTML = alert (testNaN (firstNumber) );
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+  if (stop == true) {
+      document.getElementById("answer").innerHTML = "Try Again!"
+    } else {
+
+      document.getElementById("answer").innerHTML = Solution (firstNumber);
+
+    }
+}
+
+function testNaN(number) {
+  if (isNaN (number) ) {
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "input validated"
+  }
+function Solution(firstNumber){
+  if (firstNumber/3)
+}
