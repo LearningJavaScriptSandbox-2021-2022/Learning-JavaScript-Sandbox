596a1c628c532fe6d33a65c7f0dcd26eef7ad7ad
Max Ingram <Max98620@gmail.com>
2021-10-04 19:08:05 -0600

    Update

--- scripts/DemeritPoints.js
+++ scripts/DemeritPoints.js
@@ -1,1 +1,49 @@
 console.log('You made it to the Demerit points.js!');
+
+
+let stop = false;
+
+function javascript () {
+
+}
+
+
+function main() {
+  let firstNumber, secondNumber;
+
+  firstNumber = Number(document.getElementById("textfield1").value);
+  secondNumber = Number(document.getElementById("textfield2").value);
+
+  document.getElementById("validityTest1").innerHTML = alert (testNaN (firstNumber) );
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+  document.getElementById("validityTest2").innerHTML = alert (testNaN (secondNumber) );
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+  if (stop == true) {
+      document.getElementById("answer").innerHTML = "Try Again!"
+    } else {
+
+      document.getElementById("answer").innerHTML = Solution (firstNumber, secondNumber);
+
+    }
+}
+
+function testNaN(number) {
+  if (isNaN (number) ) {
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "input validated"
+  }
+}
+
+function Solution(firstNumber, secondNumber) {
+  var speed = secondNumber - firstNumber
+  if (speed >= 60) {
+    return "bad"
+  }else if (speed < 0){
+    return "no"
+  } else {
+    var speedTwo = Math.floor(speed/5)
+    return speedTwo
+  }
+}
