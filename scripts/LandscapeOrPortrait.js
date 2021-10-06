81d78ecc3e2c3281865628ffec39062c0c94b895
Max Ingram <Max98620@gmail.com>
2021-10-05 19:25:21 -0600

    Update

--- scripts/LandscapeOrPortrait.js
+++ scripts/LandscapeOrPortrait.js
@@ -2,19 +2,12 @@ console.log('You made it to the Landscape or portrait.js!');
 
 let stop = false;
 
-function javascript () {
-
-}
-
 function main() {
-  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
-   //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
+  let firstNumber, secondNumber;
   firstNumber = document.getElementById("textField1").value;
   secondNumber = document.getElementById("textField2").value;
-  //validate by alert or html text in p tag
-  document.getElementById("validityTest1").innerHTML = alert (testNAN(firstNumber));
+
   document.getElementById("validityTest1").innerHTML = (testNAN(firstNumber));
-  document.getElementById("validityTest2").innerHTML = alert (testNAN(secondNumber));
   document.getElementById("validityTest2").innerHTML = (testNAN(secondNumber));
   if (stop == true) {
       document.getElementById("answer").innerHTML = "Try Again!"
@@ -27,17 +20,17 @@ function main() {
 
 function Solution (firstNumber, secondNumber) {
   if (firstNumber > secondNumber) {
-    return "You are in Portrait mode!"
+    return "Portrait mode!"
   }else if (firstNumber < secondNumber) {
-    return "You are in Landscape Mode!"
+    return "Landscape Mode!"
 
   }else {
-    return "You are in Square Mode!"
+    return "Square Mode!"
   }
 }
 
 function testNAN (number) {
-  if ( isNaN (number)) {//NaN are not values o REAL number system
+  if ( isNaN (number)) {
     stop = true;
     return "Type a Real Number!"
   } else {
